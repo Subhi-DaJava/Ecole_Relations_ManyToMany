0d5cd37621268dfc8e99a77a1ff1b854b149036b
Subhi YD <subhy809fr@gmail.com>
2022-05-13 08:25:52 +0200

    v1.0.0 Projet initialisé

---
+++ src/test/java/com/weten/ecole/EcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weten.ecole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
